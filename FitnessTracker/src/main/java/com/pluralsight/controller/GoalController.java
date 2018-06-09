4fc4a87e99c201dd32fa94b5a68599522881dd5c
Divij <divij.shrivastava@gmail.com>
2018-06-10 03:39:58 +0530

    Adding Trifle comment.

--- FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
@@ -29,8 +29,8 @@ public class GoalController {
 	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
 		
 		System.out.println("result has errors: " + result.hasErrors());
-		
 		System.out.println("Goal set: " + goal.getMinutes());
+		//Adding Trifle comments.
 		
 		if(result.hasErrors()) {
 			return "addGoal";
